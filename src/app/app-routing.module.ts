26e09e31e5a1b71ed068ea98b1ca5bbbf1f353d7
giovhega <gio19gar@gmail.com>
2022-09-02 23:14:13 -0500

    final sesión 4 - router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,13 @@
 import {NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { PageRoutingModule } from './pages/page-routing.module';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent},
-      { path: 'progress', component: ProgressComponent},
-      { path: 'grafica1', component: Grafica1Component},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-  },
-
-  { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent},
+//  { path: 'dashboard', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+ // { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: '**', component: NopagefoundComponent},
 ];
 
@@ -31,7 +17,9 @@ const routes: Routes = [
     RouterModule.forRoot(routes)
   ],
   exports: [
-    RouterModule
+    RouterModule,
+    AuthRoutingModule,
+    PageRoutingModule
   ]
 })
 
